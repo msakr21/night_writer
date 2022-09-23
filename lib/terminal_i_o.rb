66375a187cb743fd427d0e162df39f5d707e29d2
Mostafa Sakr <msakr3@gmail.com>
2022-09-23 13:18:05 -0600

    Fix issue with pry and rspec not reading file output.

--- lib/terminal_i_o.rb
+++ lib/terminal_i_o.rb
@@ -9,18 +9,21 @@ class TerminalIO
 
   def self.night_writer
     terminal_i_o = new
-    ARGV == ["message.txt", "braille.txt"]
     set_input_and_output(ARGV[0], ARGV[1], terminal_i_o)
     terminal_message(ARGV[1], terminal_i_o)
+    terminal_i_o
   end
 
   def self.set_input_and_output(input, output, terminal_i_o)
    terminal_i_o.message = File.read(input)
-   terminal_i_o.output = File.new(output, 'w')
+   terminal_i_o.output = File.new(output, 'w+')
+   terminal_i_o.output.puts(terminal_i_o.message)
+   terminal_i_o.output.close
+  #  require 'pry';binding.pry
   end
 
   def self.terminal_message(file_name, terminal_i_o)
-    terminal_i_o.output.puts(terminal_i_o.message.upcase)
+    # terminal_i_o.output.puts(terminal_i_o.message)
     puts "Created '#{file_name}' containing #{terminal_i_o.message.length} characters"
   end
 end
--- spec/terminal_i_o_spec.rb
+++ spec/terminal_i_o_spec.rb
@@ -1,2 +1,28 @@
+require 'rspec'
 require './lib/terminal_i_o'
 
+RSpec.describe TerminalIO do
+  let(:terminal_i_o) {TerminalIO.new}
+  
+  it "1. exists" do
+    expect(terminal_i_o).to be_a(TerminalIO)
+  end
+
+  it "2. has 2 readable attributes message and output that are nil by default" do
+    expect(terminal_i_o.message).to eq(nil)
+    expect(terminal_i_o.output).to eq(nil)
+  end
+
+  it "3. sets command line terminal input and output to the message and output attributes respectively" do
+    # require 'pry';binding.pry
+    
+    TerminalIO.set_input_and_output
+    
+    expect(terminal_i_o.message).to eq("Bob Ross Quotes")
+    expect(terminal_i_o.output).to be_a(File)
+    expect(File.read(terminal_i_o.output)).to eq("Bob Ross Quotes\n")
+  end
+
+  it "4. puts out a terminal message that says which file was created and how much" do
+  end
+end
