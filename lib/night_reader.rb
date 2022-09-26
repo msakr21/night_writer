e8be64b26812a760aab0b84c6c762bc26d7f92e0
Mostafa Sakr <msakr3@gmail.com>
2022-09-26 15:12:18 -0600

    Update night_reader method in terminal_i_o class and create night_reader.rb

---
+++ lib/night_reader.rb
@@ -0,0 +1,8 @@
+require './lib/terminal_i_o'
+require './lib/braille_character'
+require './lib/dictionary'
+require './lib/translator'
+require './lib/reverse_translator'
+
+
+TerminalIO.night_reader
