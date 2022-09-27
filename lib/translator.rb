b802750bc7f3259f98374291e16665d7d496278d
Mostafa Sakr <msakr3@gmail.com>
2022-09-27 14:11:37 -0600

    Update translator tests, update some method names, do some minor refactor.

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,6 +1,5 @@
 class Translator
-  attr_reader :check_uppercase,
-              :dictionary,
+  attr_reader :dictionary,
               :uppercase_row_1,
               :uppercase_row_2,
               :uppercase_row_3,
@@ -10,12 +9,9 @@ class Translator
               :output_row_3,
               :number_row_1,
               :number_row_2,
-              :number_row_3,
-              :check_numbers
+              :number_row_3
 
   def initialize
-    @check_uppercase = "abcdefghijklmnopqrstuvwxyz".upcase
-    @check_numbers = "0123456789"
     @dictionary = Dictionary.load_dictionary
     @uppercase_row_1 = ".."
     @uppercase_row_2 = ".."
@@ -30,22 +26,11 @@ class Translator
   end
 
   def is_uppercase?(input)
-    check_uppercase.include?(input)
+    "abcdefghijklmnopqrstuvwxyz".upcase.include?(input)
   end
 
   def is_number?(input)
-    check_numbers.include?(input)
-  end
-  
-  def upper_case_braille(input)
-    @output_row_1 += uppercase_row_1 
-    @output_row_2 += uppercase_row_2 
-    @output_row_3 += uppercase_row_3 
-    line_width_control
-    @output_row_1 += dictionary.braille_characters[input.downcase].row_1.chomp
-    @output_row_2 += dictionary.braille_characters[input.downcase].row_2.chomp
-    @output_row_3 += dictionary.braille_characters[input.downcase].row_3.chomp
-    line_width_control
+    "0123456789".include?(input)
   end
 
   def regular_braille(input)
@@ -53,6 +38,35 @@ class Translator
     @output_row_2 += dictionary.braille_characters[input].row_2.chomp
     @output_row_3 += dictionary.braille_characters[input].row_3.chomp
     line_width_control
+  end
+
+  def update_final_output
+    @final_output += (output_row_1 + "\n" + output_row_2 + "\n" + output_row_3 + "\n")
+  end
+
+  def reset_output_rows
+    @output_row_1 = ""
+    @output_row_2 = ""
+    @output_row_3 = ""
+  end
+
+  def final_output_reset
+    @final_output = ""
+  end
+
+  def line_width_control
+    if output_row_3.length >= 80
+      update_final_output
+      reset_output_rows
+    end
+  end
+
+  def upper_case_braille(input)
+    @output_row_1 += uppercase_row_1 
+    @output_row_2 += uppercase_row_2 
+    @output_row_3 += uppercase_row_3 
+    line_width_control
+    regular_braille(input.downcase)
   end
 
   def braille_number(input)
@@ -73,30 +87,9 @@ class Translator
       end
   end
 
-  def update_output
-    @final_output += (output_row_1 + "\n" + output_row_2 + "\n" + output_row_3 + "\n")
-  end
-
-  def reset_output_rows
-    @output_row_1 = ""
-    @output_row_2 = ""
-    @output_row_3 = ""
-  end
-
-  def final_output_reset
-    @final_output = ""
-  end
-
   def total_reset
     reset_output_rows
     final_output_reset
-  end
-
-  def line_width_control
-    if output_row_3.length >= 80
-      update_output
-      reset_output_rows
-    end
   end
 
   def to_braille_sentence(input)
@@ -104,7 +97,7 @@ class Translator
     input.split(//).each do |character|
       to_braille(character) if character != "\n"
     end
-    update_output if output_row_3 != ""
+    update_final_output if output_row_3 != ""
     final_output
   end
 end
